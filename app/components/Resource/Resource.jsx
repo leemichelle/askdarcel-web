e89d9af6fd1ef43f5cc5a4436522e2076835f3c1
michellecarey <genovese28@yahoo.com>
2018-09-10 11:45:33 -0700

    fixes: #340

--- app/components/Resource/Resource.jsx
+++ app/components/Resource/Resource.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import PropTypes from 'prop-types';
-import { AddressInfo, TodaysHours, PhoneNumber, ResourceCategories, Website, Email, StreetView } from './ResourceInfos';
+import {
+  AddressInfo,
+  TodaysHours,
+  PhoneNumber,
+  ResourceCategories,
+  Website,
+  Email,
+  StreetView,
+} from './ResourceInfos';
 import DetailedHours from './DetailedHours';
 import Services from './Services';
 import Notes from './Notes';
@@ -14,7 +22,8 @@ import ReactMarkdown from 'react-markdown';
 function scrollToElement(selector) {
   const elem = document.getElementById(selector);
   if (elem) {
-    elem.scrollIntoView({ block: 'start', behaviour: 'smooth' });
+    const scrollOptions = { block: 'center', behaviour: 'smooth' };
+    elem.scrollIntoView(scrollOptions);
   }
 }
 
@@ -26,6 +35,7 @@ class Resource extends Component {
   }
 
   componentDidMount() {
+    console.log(this.props.location);
     this.loadResourceFromServer();
   }
 
@@ -33,146 +43,162 @@ class Resource extends Component {
     const { query } = this.props.location;
     const resourceID = query.id;
     const url = `/api/resources/${resourceID}`;
-    fetch(url, { credentials: 'include' }).then(r => r.json())
-    .then((data) => {
-      this.setState({ resource: data.resource });
-    });
+    fetch(url, { credentials: 'include' })
+      .then(r => r.json())
+      .then((data) => {
+        this.setState({ resource: data.resource });
+      });
   }
 
   verifyResource() {
     const changeRequest = { verified_at: new Date().toISOString() };
-    dataService.post(`/api/resources/${this.state.resource.id}/change_requests`, { change_request: changeRequest })
+    dataService
+      .post(`/api/resources/${this.state.resource.id}/change_requests`, {
+        change_request: changeRequest,
+      })
       .then((response) => {
         // TODO: Do not use alert() for user notifications.
         if (response.ok) {
-          alert('Resource verified. Thanks!');  // eslint-disable-line no-alert
+          alert('Resource verified. Thanks!'); // eslint-disable-line no-alert
         } else {
-          alert('Issue verifying resource. Please try again.');  // eslint-disable-line no-alert
+          alert('Issue verifying resource. Please try again.'); // eslint-disable-line no-alert
         }
       });
   }
 
   render() {
     const { resource } = this.state;
-    return (!resource || !window.google ? <Loader /> :
-    <div className="org-container">
-      <article className="org" id="resource">
-        <div className="org--map">
-          <ResourceMap
-            name={resource.name}
-            lat={resource.address.latitude}
-            long={resource.address.longitude}
-            userLocation={this.props.userLocation}
-          />
-          <StreetView address={resource.address} resourceName={resource.name} />
-        </div>
-        <div className="org--main">
-          <div className="org--main--left">
-
-            <header className="org--main--header">
-              {
-                resource.certified &&
-                <img
-                  className="certified"
-                  src={HAPcertified}
-                  alt="Verified by the Homeless Assistance Project"
-                />
-              }
-              <h1 className="org--main--header--title">{resource.name}</h1>
-              <div className="org--main--header--rating disabled-feature">
-                <p className="excellent">
-                  <i className="material-icons">sentiment_very_satisfied</i>
-                  <i className="material-icons">sentiment_very_satisfied</i>
-                  <i className="material-icons">sentiment_very_satisfied</i>
-                  <i className="material-icons">sentiment_very_satisfied</i>
-                  <i className="material-icons">sentiment_very_satisfied</i>
-                </p>
-              </div>
-              <div className="org--main--header--hours">
-                <TodaysHours schedule_days={resource.schedule.schedule_days} />
-              </div>
-              <div className="org--main--header--phone">
-                <PhoneNumber phones={resource.phones} />
-              </div>
-              <div className="org--main--header--description">
-                <header>About this resource</header>
-                <ReactMarkdown source={resource.long_description || resource.short_description || 'No Description available'} />
-              </div>
-            </header>
-
-            <section className="service--section" id="services">
-              <header className="service--section--header">
-                <h4>Services</h4>
-              </header>
-              <ul className="service--section--list">
-                <Services description={resource.long_description} services={resource.services} />
-              </ul>
-            </section>
-
-            <Notes notes={this.state.resource.notes} />
-
-            <section className="info--section" id="info">
-              <header className="service--section--header">
-                <h4>Info</h4>
-              </header>
-              <ul className="info">
-                <div className="info--column">
-                  <ResourceCategories categories={resource.categories} />
-                  <AddressInfo address={resource.address} />
-                  <PhoneNumber phones={resource.phones} />
-                  <Website website={resource.website} />
-                  <Email email={resource.email} />
-                </div>
-                <div className="info--column">
-                  <DetailedHours schedule={resource.schedule.schedule_days} />
-                </div>
-              </ul>
-            </section>
+    return !resource || !window.google ? (
+      <Loader />
+    ) : (
+      <div className="org-container">
+        <article className="org" id="resource">
+          <div className="org--map">
+            <ResourceMap
+              name={resource.name}
+              lat={resource.address.latitude}
+              long={resource.address.longitude}
+              userLocation={this.props.userLocation}
+            />
+            <StreetView address={resource.address} resourceName={resource.name} />
           </div>
+          <div className="org--main">
+            <div className="org--main--left">
+              <header className="org--main--header">
+                {resource.certified && (
+                  <img
+                    className="certified"
+                    src={HAPcertified}
+                    alt="Verified by the Homeless Assistance Project"
+                  />
+                )}
+                <h1 className="org--main--header--title">{resource.name}</h1>
+                <div className="org--main--header--rating disabled-feature">
+                  <p className="excellent">
+                    <i className="material-icons">sentiment_very_satisfied</i>
+                    <i className="material-icons">sentiment_very_satisfied</i>
+                    <i className="material-icons">sentiment_very_satisfied</i>
+                    <i className="material-icons">sentiment_very_satisfied</i>
+                    <i className="material-icons">sentiment_very_satisfied</i>
+                  </p>
+                </div>
+                <div className="org--main--header--hours">
+                  <TodaysHours schedule_days={resource.schedule.schedule_days} />
+                </div>
+                <div className="org--main--header--phone">
+                  <PhoneNumber phones={resource.phones} />
+                </div>
+                <div className="org--main--header--description">
+                  <header>About this resource</header>
+                  <ReactMarkdown
+                    source={
+                      resource.long_description ||
+                      resource.short_description ||
+                      'No Description available'
+                    }
+                  />
+                </div>
+              </header>
 
-          <div className="org--aside">
-            <div className="org--aside--content">
-              <a
-                href={`https://maps.google.com?saddr=Current+Location&daddr=${resource.address.latitude},${resource.address.longitude}&dirflg=w`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="org--aside--content--button directions-button"
-              >
-                Get Directions
-              </a>
-              <Link to={{ pathname: '/resource/edit', query: { resourceid: resource.id } }} className="org--aside--content--button edit-button">
+              <section className="service--section" id="services">
+                <header className="service--section--header">
+                  <h4>Services</h4>
+                </header>
+                <Services description={resource.long_description} services={resource.services} />
+              </section>
+
+              {/* <Notes notes={this.state.resource.notes} /> */}
+
+              <section className="info--section" id="info">
+                <header className="service--section--header">
+                  <h4>Info</h4>
+                </header>
+                <ul className="info">
+                  <div className="info--column">
+                    <ResourceCategories categories={resource.categories} />
+                    <AddressInfo address={resource.address} />
+                    <PhoneNumber phones={resource.phones} />
+                    <Website website={resource.website} />
+                    <Email email={resource.email} />
+                  </div>
+                  <div className="info--column">
+                    <DetailedHours schedule={resource.schedule.schedule_days} />
+                  </div>
+                </ul>
+              </section>
+            </div>
+
+            <div className="org--aside">
+              <div className="org--aside--content">
+                <a
+                  href={`https://maps.google.com?saddr=Current+Location&daddr=${
+                    resource.address.latitude
+                  },${resource.address.longitude}&dirflg=w`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="org--aside--content--button directions-button"
+                >
+                  Get Directions
+                </a>
+                <Link
+                  to={{ pathname: '/resource/edit', query: { resourceid: resource.id } }}
+                  className="org--aside--content--button edit-button"
+                >
                   Make Edits
-              </Link>
-              <button
-                className="org--aside--content--button"
-                onClick={this.verifyResource}
-              >
-                Mark Info as Correct
-              </button>
-              <nav className="org--aside--content--nav">
-                <ul>
-                  <li><a href="#resource">{resource.name}</a></li>
-                  <li><a href="#services">Services</a>
-                    <ul className="service--nav--list">
-                      {
-                        resource.services.map(service => (
+                </Link>
+                <button className="org--aside--content--button" onClick={this.verifyResource}>
+                  Mark Info as Correct
+                </button>
+                <nav className="org--aside--content--nav">
+                  <ul>
+                    <li>
+                      <a href="#resource">{resource.name}</a>
+                    </li>
+                    <li>
+                      <a href="#services">Services</a>
+                      <ul className="service--nav--list">
+                        {resource.services.map(service => (
                           <li key={service.id}>
-                            <a href={`#service-${service.id}`} onClick={scrollToElement(`service-${service.id}`)}>
+                            <a
+                              href={`#service-${service.id}`}
+                              onClick={scrollToElement(`service-${service.id}`)}
+                            >
                               {service.name}
                             </a>
                           </li>
-                        ))
-                      }
-                    </ul>
-                  </li>
-                  <li><a href="#info">Info</a></li>
-                </ul>
-              </nav>
+                        ))}
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="#info">Info</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             </div>
           </div>
-        </div>
-      </article>
-    </div>
+        </article>
+      </div>
     );
   }
 }
--- app/components/Resource/Services.js
+++ app/components/Resource/Services.js
@@ -6,32 +6,23 @@ import DetailedHours from './DetailedHours';
 class Services extends Component {
   constructor(props) {
     super(props);
-    this.renderServices.bind(this);
     this.renderServicesSection.bind(this);
   }
 
   renderServicesSection() {
-    if(this.props.services && this.props.services.length > 0) {
+    if (this.props.services && this.props.services.length > 0) {
       return (
-        <div>
-          {this.renderServices(this.props.services)}
-        </div>
+        <ul className="service--section--list">
+          {this.props.services.map((service, i) => (
+            <Service service={service} key={i} />
+          ))}
+        </ul>
       );
     }
   }
 
-  renderServices(services) {
-    return services.map((service, i) => {
-      return <Service service={service} key={i} />
-    });
-  }
-
   render() {
-    return (
-      <div className="services-container">
-        {this.renderServicesSection()}
-      </div>
-    );
+    return this.renderServicesSection();
   }
 }
 
@@ -48,77 +39,96 @@ class Service extends Component {
 
   render() {
     const { infoHidden } = this.state;
-    const { service } = this.props
+    const { service } = this.props;
 
     return (
-		<li className="service" id={`service-${this.props.service.id}`} >
-			<div className="service--meta disabled-feature">
-				<p><ServiceCategory category={this.props.service.category} /></p>
-				<p>updated {this.props.service.updated_date}</p>
-			</div>
-      <h2 className="service--header">{this.props.service.name}</h2>
-      <ReactMarkdown className="service--description" source={this.props.service.long_description} />
-      <div className="service--details-toggle" onClick={this.toggleVisible}>
-        <span>{infoHidden ?
-            <span>More Info <i className="material-icons">keyboard_arrow_down</i></span> :
-            null}</span>
-      </div>
-
-      { infoHidden ? null :
-        <div className="service-application-process-container">
-          <ul className="service--details">
-            <ServiceContactDetails email={service.email}/>
-            <ServiceEligibility subject='How to apply' result={this.props.service.application_process}/>
-            <ServiceEligibility subject='Eligibilities' result={this.props.service.eligibility}/>
-            <ServiceEligibility subject='Required documents' result={this.props.service.required_documents}/>
-            <ServiceEligibility subject='Fees' result={this.props.service.fee}/>
-            {this.props.service.notes.length ? <Notes notes={this.props.service.notes}/> : null  }
-            <WeeklyHours schedule={this.props.service.schedule} />
-          </ul>
-          <div className="service--details-toggle" onClick={this.toggleVisible}>
-            <span>{infoHidden ?
-                null :
-                <span>Less Info <i className="material-icons">keyboard_arrow_up</i></span>}</span>
-          </div>
+      <li className="service" id={`service-${this.props.service.id}`}>
+        <div className="service--meta disabled-feature">
+          <p>
+            <ServiceCategory category={this.props.service.category} />
+          </p>
+          <p>updated {this.props.service.updated_date}</p>
         </div>
-    	}
+        <h2 className="service--header">{this.props.service.name}</h2>
+        <ReactMarkdown
+          className="service--description"
+          source={this.props.service.long_description}
+        />
+        <div className="service--details-toggle" onClick={this.toggleVisible}>
+          <span>
+            {infoHidden ? (
+              <span>
+                More Info <i className="material-icons">keyboard_arrow_down</i>
+              </span>
+            ) : null}
+          </span>
+        </div>
 
-    </li>
+        {infoHidden ? null : (
+          <div className="service-application-process-container">
+            <ul className="service--details">
+              <ServiceContactDetails email={service.email} />
+              <ServiceEligibility
+                subject="How to apply"
+                result={this.props.service.application_process}
+              />
+              <ServiceEligibility subject="Eligibilities" result={this.props.service.eligibility} />
+              <ServiceEligibility
+                subject="Required documents"
+                result={this.props.service.required_documents}
+              />
+              <ServiceEligibility subject="Fees" result={this.props.service.fee} />
+              {this.props.service.notes.length ? <Notes notes={this.props.service.notes} /> : null}
+              <WeeklyHours schedule={this.props.service.schedule} />
+            </ul>
+            <div className="service--details-toggle" onClick={this.toggleVisible}>
+              <span>
+                {infoHidden ? null : (
+                  <span>
+                    Less Info <i className="material-icons">keyboard_arrow_up</i>
+                  </span>
+                )}
+              </span>
+            </div>
+          </div>
+        )}
+      </li>
     );
   }
 }
 
 class WeeklyHours extends Component {
-	render() {
-		return this.props.schedule.schedule_days.length > 0 ? (
-			<li className="service--details--item">
-				<header>Hours</header>
-				<div className="service--details--item--info"><DetailedHours schedule={this.props.schedule.schedule_days} /></div>
-			</li>
-		) : null;
-	}
+  render() {
+    return this.props.schedule.schedule_days.length > 0 ? (
+      <li className="service--details--item">
+        <header>Hours</header>
+        <div className="service--details--item--info">
+          <DetailedHours schedule={this.props.schedule.schedule_days} />
+        </div>
+      </li>
+    ) : null;
+  }
 }
 
-
 class ServiceCategory extends Component {
-	render() {
-		return (
-			<span>{this.props.category}</span>
-		);
-	}
+  render() {
+    return <span>{this.props.category}</span>;
+  }
 }
 
 class ServiceContactDetails extends Component {
-  render () {
-    const { email } = this.props
+  render() {
+    const { email } = this.props;
     return email ? (
       <li className="service--details--item">
         <header>Contact Info</header>
         <div className="service--details--item--info">
-          <p>Email: <a href={'mailto:' + email}>{email}</a></p>
+          <p>
+            Email: <a href={`mailto:${email}`}>{email}</a>
+          </p>
         </div>
       </li>
-    ) : null
+    ) : null;
   }
 }
 
@@ -139,7 +149,9 @@ class ServiceEligibilities extends Component {
       <li className="service--details--item">
         <header>{this.props.subject}</header>
         <div className="service--details--item--info">
-          {this.props.eligibilities.map(eligibility => <p>{eligibility.name}</p>)}
+          {this.props.eligibilities.map(eligibility => (
+            <p>{eligibility.name}</p>
+          ))}
         </div>
       </li>
     ) : null;
@@ -148,9 +160,9 @@ class ServiceEligibilities extends Component {
 
 class Notes extends Component {
   render() {
-    let notes = this.props.notes ?  this.props.notes.map((note, i) => {
-      return <Note note={note} key={i} />
-    }) : [];
+    const notes = this.props.notes
+      ? this.props.notes.map((note, i) => <Note note={note} key={i} />)
+      : [];
 
     return (
       <li className="service--details--item">
@@ -163,9 +175,7 @@ class Notes extends Component {
 
 class Note extends Component {
   render() {
-    return (
-      <li className="services--details--notes-list--item">{this.props.note.note}</li>
-    );
+    return <li className="services--details--notes-list--item">{this.props.note.note}</li>;
   }
 }
 
