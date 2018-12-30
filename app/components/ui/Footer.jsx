fb24be4cc798f01e002b85c940334d4f6cb7c5d4
alexanderturinske <a.turinske@gmail.com>
2018-12-29 18:22:55 -0800

    ISSUE #606: Refactor footer to include more information
    Closes #606

--- app/components/ui/Footer.jsx
+++ app/components/ui/Footer.jsx
@@ -5,14 +5,40 @@ function Footer() {
   return (
     <footer className="site-footer" role="contentinfo">
       <div className="footer-content">
-        <div className="footer-text">
-            ©{ new Date().getFullYear() } Shelter Tech
+        <div className="footer-top">
+          <section className="footer-service-guide">
+            SF SERVICE GUIDE
+          </section>
+          <section className="footer-links">
+            <ul>
+              <li>ABOUT US</li>
+              <li><a href="sheltertech.org">Our Story</a></li>
+              <li><a href="sheltertech.org/volunteer">Volunteer</a></li>
+              <li><a href="sheltertech.org/donate">Donate</a></li>
+            </ul>
+            <ul>
+              <li>HELP</li>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">Digital Literacy</a></li>
+            </ul>
+            <ul>
+              <li>CONNECT</li>
+              <li><a href="info@sheltertech.org">Email</a></li>
+              <li><a href="https://www.facebook.com/ShelterTechOrg/">Facebook</a></li>
+              <li><a href="https://twitter.com/sheltertechorg">Twitter</a></li>
+              <li><a href="https://www.instagram.com/shelter_tech/">Instagram</a></li>
+            </ul>
+            <ul>
+              <li>LEGAL</li>
+              <li><Link to="/terms-of-service">Terms of Service</Link></li>
+              <li><Link to="/privacy-policy">Privacy Policy</Link></li>
+              <li><a href="#">API Policy</a></li>
+            </ul>
+          </section>
         </div>
-        <ul className="footer-links">
-          <li><a href="mailto:info@sheltertech.org">Contact Us</a></li>
-          <li><Link to="/privacy-policy">Privacy Policy</Link></li>
-          <li><Link to="/terms-of-service">Terms of Service</Link></li>
-        </ul>
+        <div className="footer-text">
+          Created and maintained by ©{ new Date().getFullYear() } ShelterTech, a 501(c)(3) nonprofit | Made with love in San Francisco
+        </div>
       </div>
     </footer>
   );
