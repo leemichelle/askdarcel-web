dd6b9994555958e690a947b6e5eb62d0546be27c
alexanderturinske <a.turinske@gmail.com>
2019-01-06 00:37:41 -0800

    ISSUE #606: Update footer
    - add footer icon
    - use BEM CSS classes
    - add h1 tags instead of li

--- app/components/ui/Footer.jsx
+++ app/components/ui/Footer.jsx
@@ -4,39 +4,42 @@ import { Link } from 'react-router';
 function Footer() {
   return (
     <footer className="site-footer" role="contentinfo">
-      <div className="footer-content">
-        <div className="footer-top">
-          <section className="footer-service-guide">
-            SF SERVICE GUIDE
+      <div className="site-footer__content">
+        <div className="site-footer__top">
+          <section className="service-guide">
+            <div className="service-guide__icon" />
+            <h1 className="service-guide__text">
+              SF Service Guide
+            </h1>
           </section>
-          <section className="footer-links">
+          <section className="site-footer__links">
             <ul>
-              <li>ABOUT US</li>
+              <h1>About Us</h1>
               <li><a href="sheltertech.org">Our Story</a></li>
               <li><a href="sheltertech.org/volunteer">Volunteer</a></li>
               <li><a href="sheltertech.org/donate">Donate</a></li>
             </ul>
             <ul>
-              <li>HELP</li>
+              <h1>Help</h1>
               <li><a href="#">FAQ</a></li>
               <li><a href="#">Digital Literacy</a></li>
             </ul>
             <ul>
-              <li>CONNECT</li>
+              <h1>Connect</h1>
               <li><a href="info@sheltertech.org">Email</a></li>
               <li><a href="https://www.facebook.com/ShelterTechOrg/">Facebook</a></li>
               <li><a href="https://twitter.com/sheltertechorg">Twitter</a></li>
               <li><a href="https://www.instagram.com/shelter_tech/">Instagram</a></li>
             </ul>
             <ul>
-              <li>LEGAL</li>
+              <h1>Legal</h1>
               <li><Link to="/terms-of-service">Terms of Service</Link></li>
               <li><Link to="/privacy-policy">Privacy Policy</Link></li>
               <li><a href="#">API Policy</a></li>
             </ul>
           </section>
         </div>
-        <div className="footer-text">
+        <div className="site-footer__disclosure">
           Created and maintained by ©{ new Date().getFullYear() } ShelterTech, a 501(c)(3) nonprofit | Made with love in San Francisco
         </div>
       </div>
