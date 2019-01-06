c1c97f11eda05cba0bbd6db2935f00119bcf5e57
Richard Xia <richardxia@richardxia.com>
2019-01-06 15:55:07 -0800

    Remove second instance of Navigation component.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -139,7 +139,7 @@ class App extends Component {
 
     return (
       <div>
-        <Navigation />
+        <Navigation showSearch={ this.props.location.pathname != '/' }/>
         <div className="container">
           {childrenWithProps}
         </div>
