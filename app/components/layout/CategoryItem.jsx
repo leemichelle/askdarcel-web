1aa4f9abc9980486fa61b488a6fbe88136989fe8
Richard Xia <richardxia@richardxia.com>
2019-02-20 21:27:08 -0800

    Escape query parameter before using in URL. (#629)

--- app/components/layout/CategoryItem.jsx
+++ app/components/layout/CategoryItem.jsx
@@ -4,9 +4,10 @@ import { images } from '../../assets';
 
 class CategoryItem extends React.Component {
   render() {
+    const url = `/search?refinementList[categories][0]=${encodeURIComponent(this.props.name)}`;
     return (
       <li className="category-item">
-        <Link className="category-button" to={`/search?refinementList[categories][0]=${this.props.name}`} >
+        <Link className="category-button" to={url} >
           <div className="category-button-content">
             <div className="category-button-icon">
               <img
