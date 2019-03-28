c67a7326ab3f213d4dc585269f9e7aceefa3753d
Richard Xia <richardxia@richardxia.com>
2019-03-27 20:18:39 -0700

    Fix more lint errors (#671)
    * Fix App.jsx.
    
    * Lint testcafe.
    
    * Fix lint on Navigation.jsx.
    
    * Fix lint on HomePage.
    
    * Fix lint on MapOfLocations.

--- app/pages/HomePage.jsx
+++ app/pages/HomePage.jsx
@@ -3,17 +3,13 @@ import Footer from 'components/ui/Footer';
 import LandingPageResourceBlock from 'components/ui/LandingPageResourceBlock';
 import LandingPageEligibilityBlock from 'components/ui/LandingPageEligibilityBlock';
 import Partners from 'components/ui/Partners';
-import WhiteLabel from 'components/ui/WhiteLabel';
 import FindHeader from 'components/layout/FindHeader';
 import { CategoryList } from 'components/layout/CategoryList';
 import BasicNeedsBlockConfig from 'components/ui/BasicNeedsBlockConfig';
 import LegalBlockConfig from 'components/ui/LegalBlockConfig';
 import * as ax from 'axios';
-import config from '../config';
 
 import './HomePage.scss';
-
-const subDomain = window.location.host.split('.')[0];
 
 export class HomePage extends React.Component {
   constructor() {
@@ -43,13 +39,14 @@ export class HomePage extends React.Component {
   }
 
   render() {
+    const { categories, eligibilities } = this.state;
     return (
       <div className="find-page">
         <div className="find-content-container">
           <FindHeader />
-          <CategoryList categories={this.state.categories} />
+          <CategoryList categories={categories} />
         </div>
-        <LandingPageEligibilityBlock eligibilities={this.state.eligibilities} />
+        <LandingPageEligibilityBlock eligibilities={eligibilities} />
         <LandingPageResourceBlock config={LegalBlockConfig}>
           <div className="legal-block__resources-hammer" />
         </LandingPageResourceBlock>
