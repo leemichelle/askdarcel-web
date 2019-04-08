f978c5ea5fd82e48b8d9d58b17a816f583759847
Anna Kalkanis <a.kalkanis@gmail.com>
2019-04-07 17:41:39 -0700

    #680 - make service page mobile friendly (#695)
    * make service page mobile friendly

--- app/pages/ServiceListingPage.jsx
+++ app/pages/ServiceListingPage.jsx
@@ -11,6 +11,7 @@ import {
   TableOfContactInfo,
   TableOfOpeningTimes,
   CategoryTag,
+  MobileActionBar,
 } from 'components/listing';
 import { MapOfLocations } from 'components/maps';
 import ReactMarkdown from 'react-markdown';
@@ -18,38 +19,61 @@ import Helmet from 'react-helmet';
 import 'react-tippy/dist/tippy.css';
 import { isSFServiceGuideSite } from '../utils/whitelabel';
 
-const getSidebarActions = resource => {
-  const sidebarActions = [
-    // TODO Edit should add service ID header
-    {
-      name: 'Edit',
-      icon: 'edit',
-      to: `/resource/edit?resourceid=${resource.id}`,
-    }, // TODO Update with path to /resource/:id
-    {
-      name: 'Print',
-      icon: 'print',
-      handler: () => {
-        window.print();
-      },
+const getResourceActions = resource => ({
+  // TODO Edit should add service ID header
+  edit: {
+    name: 'Edit',
+    icon: 'edit',
+    to: `/resource/edit?resourceid=${resource.id}`,
+  }, // TODO Update with path to /resource/:id
+  print: {
+    name: 'Print',
+    icon: 'print',
+    handler: () => {
+      window.print();
     },
-    // TODO Integrate with mobile share, how to handle shares
-    // { name: 'Share', icon: 'share' },
-    // { name: 'Save', icon: 'save' }, TODO We have no save mechanism yet
+  },
+  directions: {
+    name: 'Directions',
+    icon: 'directions',
+    link: `http://google.com/maps/dir/?api=1&destination=${
+      resource.address.latitude
+    },${resource.address.longitude}`,
+  },
+  phone: {
+    name: 'Call',
+    icon: 'call',
+    link: `tel:${resource.phones[0].number}`,
+  },
+  // TODO Integrate with mobile share, how to handle shares
+  // { name: 'Share', icon: 'share' },
+  // { name: 'Save', icon: 'save' }, TODO We have no save mechanism yet
+});
+
+const getSidebarActions = resource => {
+  const resourceActions = getResourceActions(resource);
+  const sidebarActions = [
+    resourceActions.edit,
+    resourceActions.print,
   ];
   if (resource.address) {
-    sidebarActions.push(
-      // TODO Directions to address, not lat/long, is much better UX
-      {
-        name: 'Directions',
-        icon: 'directions',
-        link: `http://google.com/maps/dir/?api=1&destination=${
-          resource.address.latitude
-        },${resource.address.longitude}`,
-      },
-    );
+    sidebarActions.push(resourceActions.directions);
   }
   return sidebarActions;
+};
+
+const getMobileActions = resource => {
+  const resourceActions = getResourceActions(resource);
+  const mobileActions = [
+    { ...resourceActions.edit, icon: 'edit-blue' },
+  ];
+  if (resource.address) {
+    mobileActions.unshift({ ...resourceActions.directions, icon: 'directions-blue' });
+  }
+  if (resource.phones && resource.phones.length > 0) {
+    mobileActions.unshift({ ...resourceActions.phone, icon: 'phone-blue' });
+  }
+  return mobileActions;
 };
 
 // TODO This should be serviceAtLocation
@@ -105,6 +129,7 @@ class ServicePage extends React.Component {
     const { resource, program, schedule } = service;
     const details = this.generateDetailsRows();
     const sidebarActions = getSidebarActions(resource);
+    const mobileActions = getMobileActions(resource);
     const locations = getServiceLocations(service, resource, schedule);
 
     return (
@@ -143,6 +168,7 @@ class ServicePage extends React.Component {
                     </span>
                   </p>
                 </header>
+                <MobileActionBar actions={mobileActions} />
 
                 <section className="listing--main--left--about">
                   <h2>About This Service</h2>
