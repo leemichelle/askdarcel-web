e7105e858e2cf6ba529f2f1564e2ba3a389d5019
Josh Painter <jpainter4@gmail.com>
2019-04-28 18:44:08 -0700

    migrating from React 15 to 16 (#728)

--- testing/mocha.js
+++ testing/mocha.js
@@ -11,7 +11,7 @@ require.extensions['.scss'] = () => null;
 
 const { jsdom } = require('jsdom');
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-15');
+const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 
