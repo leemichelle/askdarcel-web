63e66f95af85bcc8069f9629f50b4d7861b48ef2
tsyaeger <tsyaeger@gmail.com>
2019-05-14 23:05:31 -0700

    configures axios w base and headers

---
+++ app/api/httpClient.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// const baseURL = baseURLs[process.env.NODE_ENV];
+const baseURL = '/api';
+const api = axios.create({
+	baseURL,
+	headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+});
+export default api;
